1acb799793c2e959d6f274690d9cbbf27c258d88
smeng03 <68476325+smeng03@users.noreply.github.com>
2021-07-12 10:24:57 -0700

    Initial commit

---
+++ HealthHelper/HealthHelper/Models/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  HealthHelper
+//
+//  Created by Sabrina P Meng on 7/12/21.
+//
+
+#ifndef User_h
+#define User_h
+
+
+#endif /* User_h */
+
---
+++ HealthHelper/HealthHelper/Models/User.m
@@ -0,0 +1,8 @@
+//
+//  User.m
+//  HealthHelper
+//
+//  Created by Sabrina P Meng on 7/12/21.
+//
+
+#import <Foundation/Foundation.h>
