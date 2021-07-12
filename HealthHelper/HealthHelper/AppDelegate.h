02940c8337b5f27d22d9ed9969067586190b0cfb
smeng03 <68476325+smeng03@users.noreply.github.com>
2021-07-12 10:17:10 -0700

    Initial commit

---
+++ HealthHelper/HealthHelper/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HealthHelper
+//
+//  Created by Sabrina P Meng on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HealthHelper/HealthHelper/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  HealthHelper
+//
+//  Created by Sabrina P Meng on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
